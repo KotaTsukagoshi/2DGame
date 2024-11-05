8bb57c824fc61dd322bf07a828971fe448e24d6b
KotaTsukagoshi <uni202406_12@telework.to>
2024-11-05 13:30:06 +0900

    ドロップしたアイテムが消える様になった

--- Assets/Script/OrderManager.cs
+++ Assets/Script/OrderManager.cs
@@ -18,13 +18,27 @@ public class OrderManager : MonoBehaviour
     public void GenerateOrder()
     {
         currentOrder.Clear();
+        // itemSpritesがnullまたは空でないか確認
+        if (itemSprites == null || itemSprites.Count == 0)
+        {
+            Debug.LogError("itemSpritesが設定されていないか、空です。");
+            return;
+        }
+
         for (int i = 0; i < orderImages.Count; i++)
         {
-            //ランダムにアイテムを選んで注文リストに追加
-            Sprite randomItem = itemSprites[Random.Range(0, itemSprites.Count)];
-            currentOrder.Add(randomItem);
-            orderImages[i].sprite = randomItem;
-            orderImages[i].enabled = true;
+            if (i < itemSprites.Count) // アイテムが十分にあるか確認
+            {
+                //ランダムにアイテムを選んで注文リストに追加
+                Sprite randomItem = itemSprites[Random.Range(0, itemSprites.Count)];
+                currentOrder.Add(randomItem);
+                orderImages[i].sprite = randomItem;
+                orderImages[i].enabled = true;
+            }
+            else
+            {
+                Debug.LogWarning("アイテムが不足してます。");
+            }
         }
     }
 
